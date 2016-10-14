a572ff7b2c48383ff722158b563208eb5fda2291
maximilian-zollbrecht <MaxiZ_94@web.de>
2016-10-14 17:25:39 +0200

    Implemented jumping and moving with gravity. Not sure if this is the right way...

--- Project/MyWorld.java
+++ Project/MyWorld.java
@@ -16,6 +16,39 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1, false); 
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Spider spider = new Spider();
+        addObject(spider,157,86);
+        Platform platform = new Platform();
+        addObject(platform,109,318);
+        Platform platform2 = new Platform();
+        addObject(platform2,232,324);
+        Platform platform3 = new Platform();
+        addObject(platform3,155,324);
+        platform3.setLocation(127,318);
+        platform2.setLocation(146,318);
+        Platform platform4 = new Platform();
+        addObject(platform4,236,322);
+        platform4.setLocation(166,318);
+        Platform platform5 = new Platform();
+        addObject(platform5,308,285);
+        Platform platform6 = new Platform();
+        addObject(platform6,288,293);
+        Platform platform7 = new Platform();
+        addObject(platform7,335,285);
+        Platform platform8 = new Platform();
+        addObject(platform8,349,333);
+        platform6.setLocation(288,285);
+        platform7.setLocation(328,285);
+        platform8.setLocation(346,285);
     }
 }
---
+++ Project/Spider.java
@@ -0,0 +1,65 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Spider here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Spider extends Actor
+{
+    private int ySpeed = 0;
+    private final int Y_SPEED_MAX = 8;
+    private final int JUMP_STRENGTH = 15;
+    private boolean hasJumped = true;
+    private final int X_SPEED = 4;
+    
+    /**
+     * Act - do whatever the Spider wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        if(isDead()){
+            Greenfoot.setWorld(new MyWorld());
+        }
+        if(onGround()){
+            ySpeed = 0;
+            
+            if(Greenfoot.isKeyDown("up")){
+                hasJumped = true;
+                jump();
+            } else {
+                hasJumped = false;
+            }
+        } else {
+            if(ySpeed < Y_SPEED_MAX){
+                ySpeed++;
+            }
+            setLocation(getX(), getY()+ySpeed);
+        }
+
+        if(Greenfoot.isKeyDown("right")){
+            moveHorizontally(X_SPEED);
+        } else if(Greenfoot.isKeyDown("left")){
+            moveHorizontally(-X_SPEED);
+        }
+    }
+
+    public void moveHorizontally(int amount){
+        setLocation(getX()+amount, getY());
+    }
+
+    public void jump(){
+        ySpeed = -JUMP_STRENGTH;
+        setLocation(getX(), getY()+ySpeed);
+    }
+    
+    public boolean onGround(){
+        return getOneObjectAtOffset(0, getImage().getHeight()/2, Platform.class) != null;
+    }
+
+    public boolean isDead(){
+        return getY() >= getWorld().getHeight();
+    }
+}
