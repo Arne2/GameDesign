c460e70ff5ac23a3efa39e9063e185b031ae0b2b
maximilian-zollbrecht <MaxiZ_94@web.de>
2016-10-16 13:05:04 +0200

    update and comment

--- Project/Spider.java
+++ Project/Spider.java
@@ -8,11 +8,20 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Spider extends Actor
 {
-    private int ySpeed = 0;
-    private final int Y_SPEED_MAX = 8;
-    private final int JUMP_STRENGTH = 15;
-    private boolean hasJumped = true;
+    /** Speed when moving in X-direction in pixels per frame. **/
     private final int X_SPEED = 4;
+
+    /** Maximum falling speed in pixels per frame. **/
+    private final int Y_SPEED_MAX = 8;
+
+    /** Jumping acceleration in pixels per frame. **/
+    private final int JUMP_STRENGTH = 13;
+
+    /** current speed in the Y-direction in pixels per frame. **/
+    private int ySpeed = 0;
+
+    /** whether the spider is in the air or standing on something. Used to prevent jumping in mid-air. **/
+    private boolean inAir = true;
     
     /**
      * Act - do whatever the Spider wants to do. This method is called whenever
@@ -20,43 +29,52 @@ public class Spider extends Actor
      */
     public void act() 
     {
+        // TODO: implement death and game-over mechanic.
         if(isDead()){
             Greenfoot.setWorld(new MyWorld());
         }
-        if(onGround()){
+
+        // jumping and falling
+        Actor ground = getGround();
+        if(ground != null){
+            // stop falling
             ySpeed = 0;
-            
-            if(Greenfoot.isKeyDown("up")){
-                hasJumped = true;
-                jump();
-            } else {
-                hasJumped = false;
-            }
+            setToGround(ground);
+
+            // allow jumping
+            inAir = false;
         } else {
+            // fall when in the air
             if(ySpeed < Y_SPEED_MAX){
                 ySpeed++;
             }
-            setLocation(getX(), getY()+ySpeed);
         }
-
-        if(Greenfoot.isKeyDown("right")){
-            moveHorizontally(X_SPEED);
-        } else if(Greenfoot.isKeyDown("left")){
-            moveHorizontally(-X_SPEED);
+        if(!inAir && Greenfoot.isKeyDown("space")){
+            inAir = true;
+            ySpeed = -JUMP_STRENGTH;
         }
-    }
-
-    public void moveHorizontally(int amount){
-        setLocation(getX()+amount, getY());
-    }
-
-    public void jump(){
-        ySpeed = -JUMP_STRENGTH;
         setLocation(getX(), getY()+ySpeed);
+
+        // horizontal movement
+        if(Greenfoot.isKeyDown("right") || Greenfoot.isKeyDown("d")){
+            setLocation(getX()+X_SPEED, getY());
+        } else if(Greenfoot.isKeyDown("left") || Greenfoot.isKeyDown("a")){
+            setLocation(getX()-X_SPEED, getY());
+        }
     }
     
-    public boolean onGround(){
-        return getOneObjectAtOffset(0, getImage().getHeight()/2, Platform.class) != null;
+    /**
+     * Returns a Platform underneath the spider or null, if there is none. 
+     */
+    public Actor getGround(){
+        return getOneObjectAtOffset(0, getImage().getHeight()/2, Platform.class);
+    }
+
+    /**
+     * Places the spider above the given Actor. Does not move the spider horizontally.
+     */
+    public void setToGround(Actor ground){
+        setLocation(getX(), ground.getY() - (ground.getImage().getHeight()+getImage().getHeight())/2);
     }
 
     public boolean isDead(){
