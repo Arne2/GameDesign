d381732cee8617b963751581c75a86a11bcc91e0
Stefan Rauch <srauch@hm.edu>
2016-11-02 14:34:02 +0100

    Enemy baseclass

--- Project/Enemy.java
+++ Project/Enemy.java
@@ -1,14 +1,15 @@
 public abstract class Enemy extends LevelActor
 {
-    
-    public Enemy(int x, int y)
-    {
-        super(x, y);
-    }
-    
+	private boolean	stunned;
+	private int		healthPoints;
+
+	public Enemy(int x, int y)
+	{
+		super(x, y);
+	}
+
 	/**
-	 * Act - do whatever the Enemy wants to do. This method is called whenever
-	 * the 'Act' or 'Run' button gets pressed in the environment.
+	 * Act - do whatever the Enemy wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
 	 */
 	public abstract void act();
 }
