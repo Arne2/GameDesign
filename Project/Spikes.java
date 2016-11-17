c646890d7f10b1f2f19db1bdc55aa9c77fec142d
Arne <arne.schoentag@imagefiguren.de>
2016-11-17 15:50:29 +0100

    spikes class

---
+++ Project/Spikes.java
@@ -0,0 +1,18 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Spikes here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Spikes extends Enemy
+{
+    
+    
+    public Spikes (int x, int y){
+        super(x,y, false, false);
+        
+        
+    }
+}
