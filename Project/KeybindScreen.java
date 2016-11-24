3249401d3724f22309058d47220a6708e34095c1
Stefan Rauch <srauch@hm.edu>
2016-11-24 15:51:27 +0100

    Keybindclasses added

---
+++ Project/KeybindScreen.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class KeybindScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class KeybindScreen extends SplorrtWorld
+{
+
+    /**
+     * Constructor for objects of class KeybindScreen.
+     * 
+     */
+    public KeybindScreen()
+    {
+    }
+}
