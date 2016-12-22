75be9a7c47b6f82b9e9e810d84ded28bcc24e9cb
Arne2 <arne.schoentag@imagefiguren.de>
2016-12-22 13:45:57 +0100

    add Level4_1 (not in lvl selection to prevent merge conflicts)

---
+++ Project/Level4_1.java
@@ -0,0 +1,67 @@
+import greenfoot.GreenfootImage;
+import greenfoot.GreenfootSound;
+
+/**
+ * Write a description of class Level1 here.
+ * 
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Level4_1 extends Level
+{
+
+	public Level4_1(Spider spider)
+	{
+		super(spider);
+	}
+
+	public Level4_1()
+	{
+		this(null);
+	}
+
+	private static final GreenfootImage	background	= new GreenfootImage("Sky_blue.png");
+	private static final GreenfootSound	music		= new GreenfootSound("Constance.wav");
+
+	@Override
+	public int getStartingWeb()
+	{
+		return 200;
+	}
+
+	@Override
+	public GreenfootSound getBackgroundMusic()
+	{
+		return music;
+	}
+
+	@Override
+	public GreenfootImage getBackgroundImage()
+	{
+		return background;
+	}
+
+	@Override
+	protected int getMaxWebPossible()
+	{
+		return 900;
+	}
+
+	@Override
+	protected int getBestTimePossible()
+	{
+		return 7000;
+	}
+
+	@Override
+	public SplorrtWorld getNextLevel()
+	{
+		return new Level2_2();
+	}
+
+	@Override
+	public SplorrtWorld getCurrentLevel()
+	{
+		return new Level4_1();
+	}
+}
