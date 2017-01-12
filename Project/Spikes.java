92f008b5f9740c82cabc922a21b0a0ff8cabf019
Arne <arne.schoentag@imagefiguren.de>
2017-01-12 15:29:58 +0100

    death trap update (death fields and reverse spikes) + implementing in levels

--- Project/Spikes.java
+++ Project/Spikes.java
@@ -7,20 +7,24 @@
 public class Spikes extends Enemy
 {
 
-	public Spikes(int x, int y)
-	{
-		super(x, y, false, false);
-		setImage("Spikes32x32.png");
-	}
-	
-	@Override
-	public void act() {
-		super.act();
-	}
-	
-	@Override
-	public void damageSpider(Spider s)
-	{
-	    s.instantKill();
-	}
+    public Spikes(int x, int y, boolean reverse)
+    {
+        super(x, y, false, false);
+        if(reverse){
+            setImage("Spikes2(32x32).png");
+        } else {
+            setImage("Spikes32x32.png");
+        }
+    }
+    
+    @Override
+    public void act() {
+        super.act();
+    }
+    
+    @Override
+    public void damageSpider(Spider s)
+    {
+        s.instantKill();
+    }
 }
