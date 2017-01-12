ae1036b269d239a5d88e46be2f7e022e84636aaf
Stefan Rauch <srauch@hm.edu>
2017-01-12 15:31:18 +0100

    music volume in level selection always the same

--- Project/LevelSelection.java
+++ Project/LevelSelection.java
@@ -109,9 +109,9 @@ public class LevelSelection extends Level
 		levels.add(new LevelInfo(new Level2_3(), Platform.Type.STONE, Platform.Type.STONE, Platform.Type.COBBLE));
 		levels.add(new LevelInfo(new Level2_4(), Platform.Type.STONE, Platform.Type.STONE, Platform.Type.COBBLE));
 		levels.add(new LevelInfo(new Level2_5(), Platform.Type.STONE, Platform.Type.STONE, Platform.Type.COBBLE));
-		
+
 		levels.add(null);
-		
+
 		levels.add(new LevelInfo(new Level3_1(), Platform.Type.SAND, Platform.Type.SANDSTONE, Platform.Type.SANDSTONE));
 		levels.add(new LevelInfo(new Level3_2(), Platform.Type.SAND, Platform.Type.SANDSTONE, Platform.Type.SANDSTONE));
 		levels.add(new LevelInfo(new Level3_3(), Platform.Type.SAND, Platform.Type.SANDSTONE, Platform.Type.SANDSTONE));
@@ -125,7 +125,6 @@ public class LevelSelection extends Level
 		levels.add(new LevelInfo(new Level4_3(), Platform.Type.GRASS, Platform.Type.DIRT));
 		levels.add(new LevelInfo(new Level4_4(), Platform.Type.GRASS, Platform.Type.DIRT));
 		levels.add(new LevelInfo(new Level4_5(), Platform.Type.GRASS, Platform.Type.DIRT));
-
 
 		for (int y = 0; y < SIDE_HEIGHT + PLATFORM_DEPTH; y++)
 		{
@@ -208,6 +207,7 @@ public class LevelSelection extends Level
 		if (currentLevel != index && index >= 0 && index < levels.size() && levels.get(index) != null)
 		{
 			GreenfootSound newMusic = levels.get(index).getLevel().getBackgroundMusic();
+			newMusic.setVolume(Setting.getMusicVolume());
 
 			String newName = newMusic.toString().split(" ")[2];
 			String oldName = music.toString().split(" ")[2];
